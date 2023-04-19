daf7d52545ec01e9a4219735e2c270bc73cb870b
francienevaz <fran.vfraga@gmail.com>
2023-04-18 21:23:00 -0300

    add JS

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+const fetchPokemon = async (pokemon) => {
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    const data = await APIResponse.json();
+
+    console.log(data)
+}
+
+fetchPokemon('25');
